edaac1c4ccad163b154c966647f8d90671456bd7
Ansh-699 <anshtyagi7845@gmail.com>
2024-12-16 13:07:37 +0530

    new

--- script.js
+++ script.js
@@ -1,35 +1,152 @@
-function helloWorld() {
-    alert("Hello World");
-}
+// function helloWorld() {
+//     alert("Hello World");
+// }
 
-var pi=3.14;
-var myName="are you good";
-var answer = "yes i am ";
+// var pi=3.14;
+// var myName="are you good";
+// var answer = "yes i am ";
 
 
-document.write( "<br>" + "---------------------" + "<br>");
-document.write(pi + "<br>");
-document.write(myName + "<br>");
-document.write(answer + "<br>");
-document.write("---------------------");
-document.write ("<br>");
+// document.write( "<br>" + "---------------------" + "<br>");
+// document.write(pi + "<br>");
+// document.write(myName + "<br>");
+// document.write(answer + "<br>");
+// document.write("---------------------");
+// document.write ("<br>");
 
-function myFunction(){
-    let num = ["1","2","3","4","5","6","7","8","9","10"];
-    var carname = num[Math.floor(Math.random() * num.length)];
-    document.getElementById("demo").innerHTML = carname;
-}
-var num=10;
-console.log(num);
-var person = {
-    firstName: "John",
-    lastName: "Doe",
-    id: 5566,
-    getInfo:function(){
-        return this.firstName + " " + this.lastName;
-    }
-};
+// function myFunction(){
+//     let num = ["1","2","3","4","5","6","7","8","9","10"];
+//     var carname = num[Math.floor(Math.random() * num.length)];
+//     document.getElementById("demo").innerHTML = carname;
+// }
+// console.log(num);
+// var person = {
+//     firstName: "John",
+//     lastName: "Doe",
+//     id: 5566,
+//     getInfo:function(){
+//         return this.firstName + " " + this.lastName;
+//     }
+// };
 
-document.getElementById("demo").innerHTML=person.getInfo();
-document.write(person.name + " is " + person.age + " years old.");
-document.getElementById("demo").innerHTML = person.getinfo();
+// document.getElementById("demo").innerHTML=person.getInfo();
+// document.write(person.name + " is " + person.age + " years old.");
+// document.getElementById("demo").innerHTML = person.getinfo();
+
+
+// var a = 10;
+// function functiona(){
+//         console.log("1");
+//         function functionb(){
+//             console.log("2");
+//         }
+//         functionb();
+// }
+// functiona();
+// console.log("global");
+
+
+
+// var name1 ="john";
+// function first() {
+//     var a="hello"
+//     second();
+//     var x= a+name1;
+//     console.log(x);
+
+// }
+
+// function second(){
+//     var b ="hi"
+//     third();
+//     var z = b + name1 ;
+//     console.log(z);
+// }
+
+// function third(){
+//     var c ='hey';
+//     var z = c+name1;
+//     console.log(z);
+// }
+
+// first();
+
+
+// var age = 20 ;
+// if (age>18){
+//     document.write("you are eligible to vote");
+// }
+
+
+
+// var grade = "A";
+// document.write("<br />");
+// switch (grade){
+//     case "A":
+//         document.write("Excellent <br />");
+//         break;
+//     case "B":
+//         document.write("Good <br />");
+//         break;
+//     case "C":
+//         document.write("Fair    <br />");
+//         break;
+//     case "D":
+//         document.write("Poor <br />");
+//         break;
+//     case "F":
+//         document.write("very poor <br />");
+//         break;
+//     default:
+//         document.write("Invalid grade <br />");
+// }
+
+
+// var i ;
+// for (i=0; i<3; i++) {
+//    console.log(i);
+// }
+// var count ;
+// document.write("starting loop " + "<br />" + "<br />");
+// for (count=0; count<10; count++){
+//     document.write("current count : " + count + "<br />");
+//     document.write(" <br />");
+// }
+// document.write("loop stopped");
+
+
+
+// for ( var i=0; i<100; i++){
+//     if (i%2==0){
+//         console.log(i + " is even");
+
+//     }
+//     else{
+//         console.log(i + " is odd");
+//     }
+// }
+// var myArray = ["cat", "dog", "fish", "goat"];
+// for (var i=0; i<myArray.length; i++){
+//     console.log("I have a " + myArray[i] + " in index " + i);
+// }
+
+// var i =0;
+// while (i<=10){
+//     document.write("The number is " + i + "<br />");
+//     i++;
+// }
+// document.write("The loop is over");
+
+// function myFunction(){
+//     alert("Hello World");
+// }
+
+// function sayHello(name,age){
+//     document.write(name + " is " + age + " years old");
+// }
+// function addNumbers(a,b){
+//     var c = a+b;
+//     console.log(c);
+//     document.write(c);
+// }
+// addNumbers(10,20);
